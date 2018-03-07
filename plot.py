c40615ab1327aaa43fdc88600b935ef46714f20d
cyran <cyran.norbert97@gmail.com>
2018-03-07 21:47:54 +0000

    Add python script for plotting data

---
+++ plot.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+import argparse
+import matplotlib.pyplot as plt
+
+
+def draw(x, y):
+    plt.plot(x, y)
+    plt.xlabel('Data size')
+    plt.ylabel('Execution time [$\\mu$s]')
+    plt.title('Time complexity plot')
+    plt.grid(True)
+    plt.show()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Plot data from file')
+    parser.add_argument('file', help='file with data to plot')
+    args = parser.parse_args()
+
+    with open(args.file) as data_file:
+        data = data_file.read()
+
+    data = data.split('\n')
+
+    sizes = [row.split()[0] for row in data if row]
+    times = [row.split()[1] for row in data if row]
+
+    draw(sizes, times)
